d5bc5f0b7c28d4dccaa15427d35b54f096eaf249
Sam Bailey <sam.bailey@live.co.uk>
2021-01-04 19:07:09 +0000

    add contact tab contact and function to render it

---
+++ src/contact.js
@@ -0,0 +1,47 @@
+function renderContactTab() {
+    const contentDiv = document.getElementById("content");
+
+    const contactTab = document.createElement("div");
+    contactTab.id = "contactTab";
+    contentDiv.appendChild(contactTab);
+
+    const title = document.createElement("h1");
+    title.innerHTML = "Contact Us";
+    contactTab.appendChild(title);
+
+    const address = document.createElement("h3");
+    address.innerHTML = "Address";
+    contactTab.appendChild(address);
+
+    const addressline1 = document.createElement("h5");
+    addressline1.innerHTML = "123 Swanage Bay Road";
+    contactTab.appendChild(addressline1);
+
+    const addressline2 = document.createElement("h5");
+    addressline2.innerHTML = "Swanage";
+    contactTab.appendChild(addressline2);
+
+    const addressline3 = document.createElement("h5");
+    addressline3.innerHTML = "SW4N 4GE";
+    contactTab.appendChild(addressline3);
+
+    const phone = document.createElement("h3");
+    phone.innerHTML = "Phone";
+    contactTab.appendChild(phone);
+
+    const phoneNumber = document.createElement("h5");
+    phoneNumber.innerHTML = "+44 01258 808069";
+    contactTab.appendChild(phoneNumber);
+
+    const email = document.createElement("h3");
+    email.innerHTML = "Email";
+    contactTab.appendChild(email);
+
+    const emailAddress = document.createElement("h5");
+    emailAddress.innerHTML = "mangia@bastardone.com";
+    contactTab.appendChild(emailAddress);
+};
+
+export {
+    renderContactTab
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {renderHTML} from './renderHTML.js';
 import {renderMenuTab} from './menu.js';
+import {renderContactTab} from './contact.js';
 
 renderHTML();
 renderMenuTab();
+renderContactTab();
