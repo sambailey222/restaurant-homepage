c80c18319a50dc2dc73f155b8ca5008f4c8b75a2
Sam Bailey <sam.bailey@live.co.uk>
2021-01-06 08:00:07 +0000

    add tab switching functions/logic

--- src/index.js
+++ src/index.js
@@ -3,5 +3,35 @@ import {renderMenuTab} from './menu.js';
 import {renderContactTab} from './contact.js';
 
 renderHTML();
-renderMenuTab();
-renderContactTab();
+
+
+const contentDiv = document.getElementById("content"); 
+
+const homeBtn = document.getElementById("home");
+ homeBtn.addEventListener("click", () => homeTab());
+
+const logoBtn = document.getElementById("logo");
+logoBtn.addEventListener("click", () => homeTab());
+
+ function homeTab() {
+    contentDiv.innerHTML = "";
+    renderHTML();
+ }
+
+ 
+
+ const menuBtn = document.getElementById("menu");
+ menuBtn.addEventListener("click", () => menuTab());
+
+ function menuTab() {
+    contentDiv.innerHTML = "";
+    renderMenuTab();
+ }
+
+ const contactBtn = document.getElementById("contact");
+ contactBtn.addEventListener("click", () => contactTab());
+
+ function contactTab() {
+    contentDiv.innerHTML = "";
+    renderContactTab();
+ }
